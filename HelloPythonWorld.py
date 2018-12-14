32bc4a5fd527ea460b29f99d1945e8bcc42f992e
WanMann <Wanshane@outlook.com>
2018-12-14 16:18:24 +0900

    Add files via upload

---
+++ HelloPythonWorld.py
@@ -0,0 +1,4 @@
+print('hello, world')
+print('input your name:')
+name = input()
+print(name,'is a smart person. ')
---
+++ countYearsOld.py
@@ -0,0 +1,7 @@
+s = input('birth:')
+birth = int(s)
+if birth >= 1990 :
+    print('your age is', 2018 - birth)
+    print('you are young')
+else:
+    print('oh///')
